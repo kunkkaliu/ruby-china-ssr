5a2c8b02f067b6d533e7fc3c3d33a8ae2897d13b
kunkkaliu <kunkkaliu@foxmail.com>
2017-10-26 00:04:34 +0800

    update

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -17,16 +17,16 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false,
-        //         drop_debugger: true,
-        //         drop_console: true
-        //     },
-        //     output: {
-        //         comments: false
-        //     }
-        // })
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_debugger: true,
+                drop_console: true
+            },
+            output: {
+                comments: false
+            }
+        })
     ],
     resolve: {
         extensions: ['', '.js', '.jsx']
