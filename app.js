8317760ee912669b98ba28e51abbecdef29e4710
kunkkaliu <kunkkaliu@foxmail.com>
2017-12-08 15:23:34 +0800

    update

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     var static = /^\/static/g;
     var suffix = /\.map$/g;
-    if(static.test(req.path) && suffix.test(req.path)){
-	res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-	return res.end('请求非法');
-    }else {
-	next();
+    if (static.test(req.path) && suffix.test(req.path)) {
+        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+        return res.end('请求非法');
+    } else {
+        next();
     }
 });
 app.use('/static', express.static(path.join(__dirname, 'dist/static')));
